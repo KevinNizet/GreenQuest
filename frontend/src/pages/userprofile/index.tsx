56400e34e7cdcee009adddd980b44e6160ba0d24
KevinNizet <kevin.nizet@gmail.com>
2024-05-14 11:13:47 +0200

    :construction: Use of query mySelf

--- frontend/src/pages/userprofile/index.tsx
+++ frontend/src/pages/userprofile/index.tsx
@@ -3,6 +3,8 @@ import { useQuery } from "@apollo/client";
 import { queryGetUsers } from "@/graphql/queryGetUser";
 
 import Layout from "@/components/Layout";
+import { queryUser } from "@/graphql/queryUser";
+import { queryMySelf } from "@/graphql/queryMySelf";
 
 interface UserType {
   id: number;
@@ -13,30 +15,38 @@ interface UserType {
 }
 
 export default function Profile(): React.ReactNode {
-  const { loading, error, data } = useQuery(queryGetUsers);
+  /*  const { loading, error, data } = useQuery(queryGetUsers); */
+  const { loading, error, data } = useQuery(queryMySelf);
 
   if (loading) return <p>Chargement...</p>;
   if (error) return <p>Erreur: {error.message}</p>;
 
-  const users = data?.getUsers;
+  const me = data?.item;
 
-  console.log(users);
+  /* const users = data?.getUsers; */
+
+  console.log(me);
 
   // la const users (récupérée depuis la query getUsers) retourne un tableau d'utilisateur
   // il faut donc faire un .map
+
+  //TODO recréer un utilisateur via mutation signup sur Apollo Studio
 
   return (
     <Layout title="Mon profil">
       <main className="main-content">
         <p>Page de profil</p>
-        {users.map((user: UserType) => (
+        {me?.email}
+        {me?.id}
+        {/* {users.map((user: UserType) => (
           <div key={user.id}>
             <p>Mon adresse mail est : {user.email}</p>
             <p>Mon prénom : {user.firstname}</p>
             <p>Mon nom de famille : {user.lastname}</p>
             <p>Mon pseudo : {user.nickname}</p>
+            <p>Mon id : {user.id}</p>
           </div>
-        ))}
+        ))} */}
       </main>
     </Layout>
   );
