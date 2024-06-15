1d2b286bffbd0260ad1ac7baf2fb7057df107250
Laure Janin <laure.janin@bedrockstreaming.com>
2024-06-15 18:34:05 +0200

    ajustement sharp

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,9 +3,14 @@ import { Express, Request, Response } from "express";
 import { Image } from "./entities/Image";
 import { User } from "./entities/User";
 import { getUserFromReq } from "./auth";
+import sharp from "sharp";
+import cors from "cors";
 
 export function initializeRoutes(app: Express) {
-  const upload = multer({ dest: "/app/uploads/" });
+  const storage = multer.memoryStorage();
+  const upload = multer({ storage });
+
+  app.use("/api", cors());
 
   // Route pour uploader une image
   app.post(
@@ -29,13 +34,36 @@ export function initializeRoutes(app: Express) {
           .json({ success: false, message: "Utilisateur non trouvé" });
       }
 
-      if (req.file) {
+      if (req.file && req.file.mimetype.startsWith("image/")) {
+        // Récupération de l'extention de l'image grâce à mime
+        const extension = req.file.originalname.split(".").pop();
+        const filename = `${Date.now()}-${
+          Math.log(Math.random() * 8999) + 1000
+        }.${extension}`;
+
+        // Redimensionnement du fichier avec sharp
+        await sharp(req.file.buffer)
+          .resize({
+            width: 2000,
+            height: 2000,
+            fit: "inside",
+            withoutEnlargement: true,
+          })
+          .toFile(`/app/uploads/${filename}`);
+
+        // Enregistrement de l'image
         const newImage = new Image();
         newImage.mimetype = req.file.mimetype;
-        newImage.path = req.file.path;
+        newImage.path = `/app/uploads/${filename}`;
         newImage.originalName = req.file.originalname;
         await newImage.save();
-
+        res.json({
+          success: true,
+          image: {
+            id: newImage.id,
+            uri: newImage.uri,
+          },
+        });
         user.image = newImage;
         await user.save();
 
